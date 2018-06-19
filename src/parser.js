77dd0708d46c9a9d6d59f5a2f44b7ee5e19e97c4
Shai James <skyandbeyond@gmail.com>
2018-06-19 22:56:04 +0300

    html formatting

--- src/parser.js
+++ src/parser.js
@@ -8,19 +8,38 @@ exports.constitution = abi => {
 
 };
 
-exports.parse = (actionName, actionParameters, ricardianContract, signingAccount = null) => {
+                                                                                        // { h1:'h1', h2:'h2' }
+exports.parse = (actionName, actionParameters, ricardianContract, signingAccount = null, html = null) => {
+
+    if(html !== null){
+        if(typeof html === "boolean") html = {};
+        if(!html.hasOwnProperty('h1')) html.h1 = 'h1';
+        if(!html.hasOwnProperty('h2')) html.h2 = 'h2';
+    }
+
+    // Stripping backticks
     ricardianContract =
         ricardianContract.replace(/`/g, '');
 
+    // Replacing action name
     ricardianContract =
         replacePlaceholder(ricardianContract, actionName, actionName);
 
+    // Replacing action parameters
     Object.keys(actionParameters).map(param =>
         ricardianContract =
             replacePlaceholder(ricardianContract, param, actionParameters[param])
     );
 
+    // Replacing signer
     if(signingAccount) ricardianContract = replacePlaceholder(ricardianContract, "signer", signingAccount);
+
+    // Optional HTML formatting.
+    if(html !== null){
+        ricardianContract = ricardianContract.replace('# Action', `<${html.h1}>Action</${html.h1}>`);
+        ricardianContract = ricardianContract.replace('## Description', `<${html.h2}>Action</${html.h2}>`);
+        ricardianContract = ricardianContract.replace(/[\n\r]/g, '<br>');
+    }
 
     return ricardianContract;
 };
--- test/parser.spec.js
+++ test/parser.spec.js
@@ -7,10 +7,29 @@ describe('parser', () => {
 
     const systemAbi = require('./data/system.abi');
 
-    it('should be able to parse ricardian contracts and remove curlys; replacing them with data', () => {
-        const ricardian = systemAbi.abi.actions.find(fcAction => fcAction.name === 'bidname').ricardian_contract;
-        const parsedRicardianContract = parser.parse('bidname', {bid:'3 EOS', bidder:'testaccount', newname:'somename'}, ricardian, "testaccount");
+    const testAction = (actionName, data, signer = null, html = null) => {
+        const ricardian = systemAbi.abi.actions.find(fcAction => fcAction.name === actionName).ricardian_contract;
+        const parsedRicardianContract = parser.parse(actionName, data, ricardian, signer, html);
+        assert(parsedRicardianContract.indexOf('{{') === -1, `Could not parse the ${actionName} action`);
+        assert(parsedRicardianContract.indexOf('}}') === -1, `Could not parse the ${actionName} action`);
         console.log(parsedRicardianContract);
+        console.log('\r\n\------------------------------------------------------------------\r\n\ ');
+    };
+
+    // it('should be able to parse the newaccount action', () => {
+    //     testAction('newaccount', {creator: 'testcreator', name: 'testaccount', owner: 'somekey', active:'somekey'}, 'testcreator');
+    // });
+    //
+    // it('should be able to parse the updateauth action', () => {
+    //     testAction('updateauth', {account: 'testaccount', permission: 'subactive', parent: 'active', auth:'active'}, 'testaccount');
+    // });
+    //
+    // it('should be able to parse the bidname action', () => {
+    //     testAction('bidname', {bid: '3 EOS', bidder: 'testaccount', newname: 'somename'}, 'testaccount');
+    // })
+
+    it('should be able to parse the bidname action with html enabled', () => {
+        testAction('bidname', {bid: '3 EOS', bidder: 'testaccount', newname: 'somename'}, 'testaccount', true);
     });
 
 });
