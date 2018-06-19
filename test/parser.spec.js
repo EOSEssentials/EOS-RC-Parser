a751acc615962a738cc05aef85a012e47184bc84
Shai James <skyandbeyond@gmail.com>
2018-06-19 22:58:45 +0300

    full tests

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -12,24 +12,33 @@ describe('parser', () => {
         const parsedRicardianContract = parser.parse(actionName, data, ricardian, signer, html);
         assert(parsedRicardianContract.indexOf('{{') === -1, `Could not parse the ${actionName} action`);
         assert(parsedRicardianContract.indexOf('}}') === -1, `Could not parse the ${actionName} action`);
-        console.log(parsedRicardianContract);
-        console.log('\r\n\------------------------------------------------------------------\r\n\ ');
+        // console.log(parsedRicardianContract);
+        // console.log('\r\n\------------------------------------------------------------------\r\n\ ');
+        return parsedRicardianContract;
     };
 
-    // it('should be able to parse the newaccount action', () => {
-    //     testAction('newaccount', {creator: 'testcreator', name: 'testaccount', owner: 'somekey', active:'somekey'}, 'testcreator');
-    // });
-    //
-    // it('should be able to parse the updateauth action', () => {
-    //     testAction('updateauth', {account: 'testaccount', permission: 'subactive', parent: 'active', auth:'active'}, 'testaccount');
-    // });
-    //
-    // it('should be able to parse the bidname action', () => {
-    //     testAction('bidname', {bid: '3 EOS', bidder: 'testaccount', newname: 'somename'}, 'testaccount');
-    // })
+    it('should be able to parse the newaccount action', () => {
+        testAction('newaccount', {creator: 'testcreator', name: 'testaccount', owner: 'somekey', active:'somekey'}, 'testcreator');
+    });
+
+    it('should be able to parse the updateauth action', () => {
+        testAction('updateauth', {account: 'testaccount', permission: 'subactive', parent: 'active', auth:'active'}, 'testaccount');
+    });
+
+    it('should be able to parse the bidname action', () => {
+        testAction('bidname', {bid: '3 EOS', bidder: 'testaccount', newname: 'somename'}, 'testaccount');
+    });
 
     it('should be able to parse the bidname action with html enabled', () => {
-        testAction('bidname', {bid: '3 EOS', bidder: 'testaccount', newname: 'somename'}, 'testaccount', true);
+        const parsed =testAction('bidname', {bid: '3 EOS', bidder: 'testaccount', newname: 'somename'}, 'testaccount', true);
+        assert(parsed.indexOf('<h1>') > -1, 'did not format html');
+        assert(parsed.indexOf('<h2>') > -1, 'did not format html');
+    });
+
+    it('should be able to parse the bidname action with html enabled and custom tags', () => {
+        const parsed = testAction('bidname', {bid: '3 EOS', bidder: 'testaccount', newname: 'somename'}, 'testaccount', {h1:'b', h2:'i'});
+        assert(parsed.indexOf('<b>') > -1, 'did not format html with custom tags');
+        assert(parsed.indexOf('<i>') > -1, 'did not format html with custom tags');
     });
 
 });
